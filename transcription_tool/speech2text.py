5bc029741cba801a71818bf2444f4485efee8be6
tro16 <tim.roethig@yahoo.de>
2024-04-14 23:26:17 +0200

    creating package

--- transcription_tool/speech2text.py
+++ transcription_tool/speech2text.py
@@ -8,7 +8,7 @@ def speech2text(audio: dict, model: str = "openai/whisper-tiny", language: str =
     torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
     # load model from huggingface
-    processor = WhisperProcessor.from_pretrained(model, torch_dtype=torch_dtype)
+    processor = WhisperProcessor.from_pretrained(model)
     model = WhisperForConditionalGeneration.from_pretrained(model, torch_dtype=torch_dtype)
     model.to(device)
 
@@ -16,8 +16,8 @@ def speech2text(audio: dict, model: str = "openai/whisper-tiny", language: str =
     forced_decoder_ids = processor.get_decoder_prompt_ids(language=language, task="transcribe")
 
     # create input
-    input_features = processor(audio["raw"], sampling_rate=audio["sampling_rate"], return_tensors="pt", torch_dtype=torch_dtype).input_features
-    input_features.to(device)
+    input_features = processor(audio["raw"], sampling_rate=audio["sampling_rate"], return_tensors="pt").input_features
+    input_features = input_features.to(torch_dtype).to(device)
 
     # run inference
     predicted_ids = model.generate(input_features, forced_decoder_ids=forced_decoder_ids)
