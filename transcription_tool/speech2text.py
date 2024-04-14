87af323b5eba047461a24d36255862544c1d1e40
tro16 <tim.roethig@yahoo.de>
2024-04-14 23:07:16 +0200

    creating package

--- transcription_tool/speech2text.py
+++ transcription_tool/speech2text.py
@@ -14,6 +14,7 @@ def speech2text(audio: dict, model: str = "openai/whisper-tiny", device: str = "
 
     # create input
     input_features = processor(audio["raw"], sampling_rate=audio["sampling_rate"], return_tensors="pt").input_features
+    input_features.to(device)
 
     # run inference
     predicted_ids = model.generate(input_features, forced_decoder_ids=forced_decoder_ids)
